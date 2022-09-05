4ea6f37744e76873f8119247bbe34edf077372c6
ohhappylife <sshon2@jhu.edu>
2022-09-05 01:50:23 -0400

    .

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ except IndexError:
 
 try:
     if re.match(r'\d{4}-\d{2}-\d{2}', sys.argv[2]): since = sys.argv[2]
-    if re.match(r'\d{4}-\d{2}-\d{2}', sys.argv[3]): to = sys.argv[2]
-    if (sys.argv[4].isnumeric()): number = sys.argv[3]
+    if re.match(r'\d{4}-\d{2}-\d{2}', sys.argv[3]): to = sys.argv[3]
+    if (sys.argv[4].isnumeric()): number = sys.argv[4]
     df = extractTweets(keyword, since, to, number)
 except:
     df = extractTweets(keyword)
