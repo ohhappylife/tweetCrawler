efb9fdd63ef1ecc2f7d80d53d37d8663eec614c1
ohhappylife <sshon2@jhu.edu>
2022-09-29 21:11:59 -0400

    .

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from extract import extractTweets
 import sys
 from temp import savetoBucket
 from transform import dropColumns, removeStopwords, removeHashTagMention, removeEmoji, removeURL
-from datetime import date, timedelta
+from datetime import date
 from transform.removeRT import removeRT
+import pandas as pd
 
 try:
   keyword = sys.argv[1]
@@ -19,15 +20,21 @@ try:
 except IndexError:
   df = extractTweets.getTweet(keyword)
 
+df.to_csv('crawled_data.csv')
+
 df = dropColumns.getRequiredInformation(df)
 
-cleaned = df['full_text'].apply(removeStopwords.stopwords)
-cleaned = cleaned['full_text'].apply(removeEmoji.removeemoji)
-cleaned = cleaned['full_text'].apply(removeHashTagMention.removeHashtagMention)
-cleaned = cleaned['full_text'].apply(removeRT.removeRT)
-cleaned = cleaned['full_text'].apply(removeURL.removeURL)
+df.to_csv('raw_data.csv')
 
-savetoBucket(df, 'tweetcrawlerdata', str(date.today()) + '_' + keyword)
-savetoBucket(cleaned, 'tweetcrawlerdata', str(date.today()) + '_' + keyword)
+df['full_text'] = df['full_text'].apply(removeStopwords.stopwords)
+df['full_text'] = df['full_text'].apply(removeEmoji.removeemoji)
+df['full_text'] = df['full_text'].apply(removeHashTagMention.removeHashtagMention)
+df['full_text'] = df['full_text'].apply(removeRT)
+df['full_text'] = df['full_text'].apply(removeURL.removeURL)
 
-cleaned.create_Ngram(df, str(date.today()), 'full_text')
+df.to_csv('cleaned_data.csv')
+
+# savetoBucket(df, 'tweetcrawlerdata', str(date.today()) + '_' + keyword)
+# savetoBucket(df, 'tweetcrawlerdata', str(date.today()) + '_' + keyword)
+
+# df.create_Ngram(df, str(date.today()), 'full_text')
--- transform/removeStopwords.py
+++ transform/removeStopwords.py
@@ -1,2 +1,6 @@
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('english'))
+
 def stopwords(text):
   return " ".join([word for word in str(text).split() if word not in stop_words])
