7a538059ea3295434b4d9cf5cf2f59dcaaf408ee
ohhappylife <sshon2@jhu.edu>
2022-10-14 13:58:56 -0400

    Fix typo, add usage.

--- information.py
+++ information.py
@@ -1,16 +1,16 @@
-def getck():
+def getck(): # please put ck
   return ''
 
-def getcs():
+def getcs(): # please put cs
   return ''
 
-def getat():
+def getat(): # please put at
   return '-'
 
-def getats():
+def getats(): # please put ats
   return ''
 
-def savetoBucket(df, bn, fn):
+def savetoBucket(df, bn, fn): # dataframe to be saved, bucket name, file name
   path = "s3://" + bn + "/" + fn
   df.to_csv(path,
               storage_options={'key': '',
